3ad3152e378bf03e8b90d148615691ba6d440f8e
J-I-N-G-L-I <lj20001130@gmail.com>
2025-06-13 11:15:35 +0100

    Test passed for ROCOv2Dataset loading

--- config/config.py
+++ config/config.py
@@ -3,7 +3,7 @@ import os
 
 class TrainingConfig:
     # data configs
-    data_root: str = '../data/ROCO/rocov2'
+    data_root: str = r'C:\D\Programming\Datasets\rocov2'
     train_split: str = 'train'
     val_split: str = 'valid'
     test_split: str = 'test'
